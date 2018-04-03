b8cdb86486b18e2bdcfcc71e1d026a098339d318
Hucy <yanghc1226@outlook.com>
2018-04-03 10:05:44 +0800

    Feat: update Loop

--- loop/src/loop.ts
+++ loop/src/loop.ts
@@ -1,11 +1,32 @@
-export default class Loop {
-  private _t : number
-
-  constructor(time:number) {
-    this._t = time
+export default  class Loop {
+  stopArr: Array<Array<any>>;
+  apiFc: Function;
+  interval: number;
+  private _timer: any;
+  constructor(stopArr: Array<Array<any>>, apiFc: Function, interval: number = 3000) {
+    this.stopArr = stopArr;
+    this.apiFc = apiFc;
+    this.interval = interval;
   }
 
-  get T(){
-    return this._t
+  start() {
+    return this._start_();
   }
+
+  private async _start_() {
+    const data = await this.apiFc();
+    const shouldStop = this.stopArr.some(item => {
+      return data[item[0]] === item[1];
+    });
+    if (shouldStop) {
+      return Promise.resolve(data);
+    }
+    return new Promise(resolve => {
+      this._timer = setTimeout(() => {
+        resolve(this._start_());
+      }, this.interval);
+    });
+  }
+
 }
+
--- loop/test/loop.test.ts
+++ loop/test/loop.test.ts
@@ -1,11 +1,26 @@
-import Loop from "../src/loop";
+import Loop from '../src/loop';
+jest.setTimeout(100000);
 
-describe("loop test", () => {
-  it("Loop is instantiable", () => {
-    expect(new Loop(1)).toBeInstanceOf(Loop)
-  })
+const apiFnGen = () => {
+  let time = 0;
+  return () => {
+    time++;
+    return Promise.resolve({
+      time: time,
+    });
+  };
+}
 
-  it("Loop.T is t", () => {
-    expect(new Loop(1).T).toBe(1)
-  })
-})
+describe('loop test', () => {
+  it('Loop is instantiable', () => {
+    expect(new Loop([['time', 2]], apiFnGen())).toBeInstanceOf(Loop);
+  });
+
+  it('Loop.start return Promise ', async () => {
+    await expect(new Loop([['time', 2]], apiFnGen()).start()).resolves.toEqual({ time: 2 });
+  });
+
+  it('Loop.start return Promise with interval ', async () => {
+    await expect(new Loop([['time', 2]], apiFnGen(), 100).start()).resolves.toEqual({ time: 2 });
+  });
+});
